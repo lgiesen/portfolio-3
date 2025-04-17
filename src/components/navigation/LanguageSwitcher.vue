adc58612f4153df70a1d04fd336b794058d0f1ae
Leo Giesen <contact@leogiesen.de>
2025-04-17 09:46:54 +0200

    :art: convert pug to html

--- src/components/navigation/LanguageSwitcher.vue
+++ src/components/navigation/LanguageSwitcher.vue
@@ -1,39 +1,20 @@
-<template lang="pug">
-  //- Set the width, so that the svg does not scale to its 900x600 size
-  v-btn.pa-0(text icon @click="switchLanguage")
-    | {{ languageSwitchBtn }}
-  </template>
+<template>
+  <v-btn text icon class="pa-0" @click="switchLanguage">
+    {{ languageSwitchBtn }}
+  </v-btn>
+</template>
 
-<script setup>
-import { computed } from 'vue';
-import { useI18n } from 'vue-i18n';
+<script setup lang="ts">
+import { computed } from 'vue'
+import { useI18n } from 'vue-i18n'
 
-// Define the event emitter to notify parent components
-const emit = defineEmits(['language-changed']);
+const emit = defineEmits(['language-changed'])
+const { locale } = useI18n()
 
-// Optional languages array, if needed elsewhere
-const langs = ["de", "en"];
-
-// Access the i18n composable which provides a reactive locale reference
-const { locale } = useI18n();
-
-// Method to switch language
 function switchLanguage() {
-  if (!locale.value || locale.value === "en")
-    locale.value = "de";
-  else if (locale.value === "de")
-    locale.value = "en";
-  // Emit an event to notify the parent component (TheNavigation.vue)
-  emit("language-changed");
+  locale.value = locale.value === 'en' ? 'de' : 'en'
+  emit('language-changed')
 }
 
-// Which label to display on the button
-const languageSwitchBtn = computed(() => {
-  // one could also use images: require("@/assets/locales/English.svg") and require("@/assets/locales/German.svg")
-  if (!locale.value || locale.value === "de") {
-    return "DE";
-  } else {
-    return "EN";
-  }
-});
+const languageSwitchBtn = computed(() => (locale.value === 'de' ? 'DE' : 'EN'))
 </script>
