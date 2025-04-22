e7555926c3149e00a94bfbd56ebccc16b9ba34e9
Leo Giesen <contact@leogiesen.de>
2025-04-22 17:35:47 +0200

    :art: improve UI

--- src/components/Parallax.vue
+++ src/components/Parallax.vue
@@ -1,38 +1,35 @@
 <template>
   <section class="mb-0">
     <section class="fullwidth mb-0">
-      <v-parallax class="pt-12 position-relative" :class="parallaxHeight" :src="parallaxImage" :alt="parallaxImage"
+      <v-parallax class="position-relative" :class="parallaxHeight" :src="parallaxImage" :alt="parallaxImage"
         height="100vh">
         <!-- Farb-Overlay über das Bild -->
         <div class="position-absolute top-0 left-0 w-100 h-100"
           :style="{ backgroundColor: overlayColor, opacity: 0.7, zIndex: 1 }"></div>
 
         <!-- Inhalt über dem Overlay -->
-        <div class="position-relative" style="z-index: 2">
-          <section class="mb-0 mt-2">
-            <v-container>
-              <v-row class="pa-md-12 d-flex text-center">
-                <v-col :order-md="2">
-                  <h1 class="text-white text-h3 font-weight-light mb-8"
-                    v-html="$t(`parallaxText.${translationKey}.title`)"></h1>
-                  <h5 class="text-white text-h5 text-subtitle-1 mb-0"
-                    v-html="$t(`parallaxText.${translationKey}.subtitle`)">
-                  </h5>
-                  <ScrollHero class="mt-16 pt-16" />
-                </v-col>
+        <div class="position-relative" style="z-index: 2; height: 100%">
+          <v-container fill-height>
+            <v-row align="center" justify="center" class="text-center" style="min-height: 100vh">
+              <v-col :order-md="2">
+                <h1 class="text-white text-h3 font-weight-light mb-8"
+                  v-html="$t(`parallaxText.${translationKey}.title`)"></h1>
+                <h5 class="text-white text-h5 mb-0" v-html="$t(`parallaxText.${translationKey}.subtitle`)"></h5>
+                <ScrollHero class="mt-16 pt-16" />
+              </v-col>
 
-                <v-col v-if="smallImgPath" cols="12" md="6" :order-md="1" class="d-flex justify-center align-center">
-                  <v-img :alt="imgAlt" :src="smallImgPath" height="500" max-height="50vh" width="100%" contain
-                    class="rounded-lg" />
-                </v-col>
-              </v-row>
-            </v-container>
-          </section>
+              <v-col v-if="smallImgPath" cols="12" md="6" :order-md="1" class="d-flex justify-center align-center">
+                <v-img :alt="imgAlt" :src="smallImgPath" height="500" max-height="60vh" max-width="400" cover
+                  class="rounded-lg elevation-4" />
+              </v-col>
+            </v-row>
+          </v-container>
         </div>
       </v-parallax>
     </section>
   </section>
 </template>
+
 
 <script setup lang="ts">
 import ScrollHero from "@/components/scroll/ScrollHero.vue";
