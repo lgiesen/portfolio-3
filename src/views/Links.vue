79bce2394a43a0e35b874df02e45e2e84f60fe07
Leo Giesen <contact@leogiesen.de>
2025-07-02 18:59:25 +0200

    :sparkles: center text & rm icon

--- src/views/Links.vue
+++ src/views/Links.vue
@@ -11,17 +11,18 @@ div
                     height="350px"
                     cover)
                 v-card-title.justify-center 
-                    h1.my-1 Leo Giesen
+                    h1.my-1.text-center Leo Giesen
                 v-card-subtitle.mt-1.center-items
-                    h2.font-weight-medium.my-0.text-center {{ $t("motto") }}
-                template(v-slot:prepend)
-                    v-avatar(color="blue-darken-2")
-                        v-icon mdi-alarm
+                    h2.font-weight-medium.my-0.text-center {{ $t("home.homeHero.title") }}
+                //- template(v-slot:prepend)
+                //-     v-avatar(color="blue-darken-2")
+                //-         v-icon mdi-alarm
                 v-divider.mx-auto.my-3(width='200px')
                 v-card-text.center-items
                     Icons(:iconGroup="socialIcons")
                     Icons(:iconGroup="programmingIcons")
-    img(height='100px')
+    img(height='50px')
+    img.hidden-sm-and-down(height='100px')
 </template>
 
 <script>
@@ -36,17 +37,7 @@ export default {
   data: () => ({
     socialIcons: socialIcons,
     programmingIcons: programmingIcons,
-  }),
-  i18n: {
-    messages: {
-      en: {
-        motto: "Progress with Passion",
-      },
-      de: {
-        motto: "Fortschritt durch Leidenschaft",
-      },
-    },
-  },
+  })
 };
 </script>
 
